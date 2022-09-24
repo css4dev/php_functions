9c85bf7db7e961be747e35bd08abb49a668d7072
css4dev <102645748+css4dev@users.noreply.github.com>
2022-09-24 16:53:46 +0300

    Update firstAndEndDateOfWeekBasedOnNumOfWeekAndNumOfMonth.php

--- firstAndEndDateOfWeekBasedOnNumOfWeekAndNumOfMonth.php
+++ firstAndEndDateOfWeekBasedOnNumOfWeekAndNumOfMonth.php
@@ -1,22 +1,28 @@
-function firstAndEndDateOfWeekBasedOnNumOfWeekAndNumOfMonth($month, $weekNum, $year)
+<?php
+function firstAndEndDateOfWeek($month, $weekNum, $year)
 {
-    $firstDayOfMonth = '1-' . $month . '-' . $year;
+
+    $firstDayOfMonth = '01-' . $month . '-' . $year;
 
     $week = weekOfYear($firstDayOfMonth); //week num for first week of month
-
     $weekNumOfYear = $week + $weekNum - 1;
-
-    $firstDay = date('l', '1-1-' . $year);
-
+    $firstDay = date('l', strtotime('1-1-' . $year));
     $daysOfWeek = array('Saturday', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday');
-    //echo $firstDay.array_search($firstDay,$daysOfWeek);
-    $shift = 7 - array_search($firstDay, $daysOfWeek) + 1;
-
-    $dayOfYear = ($weekNumOfYear - 2) * 7;
-
-    $firstDayOfWeek = date('Y-m-d', strtotime("1-1-" . $year . " + " . $dayOfYear   . " days"));
-    echo "1-1-" . $year . " + " . $dayOfYear   . " days";
-    $endDayOfWeek = date('Y-m-d', strtotime($firstDayOfWeek . ' + 6 days'));
-    print_r(array($firstDayOfWeek, $endDayOfWeek));
+    $shift = 7 - (array_search($firstDay, $daysOfWeek) + 1);
+    $dayOfYear = ($weekNumOfYear - 2) * 7 + $shift;
+    $firstDayOfWeek1 = date('Y-m-d', strtotime("1-1-" . $year . " + " . $dayOfYear   . " days"));
+    if (date('m', strtotime($firstDayOfWeek1)) != $month) {
+        $firstDayOfWeek = date('Y-m-d', strtotime("1-" . $month . "-" . $year));
+    } else {
+        $firstDayOfWeek = $firstDayOfWeek1;
+    }
+    $lastDayOfMonthInt = new DateTime($firstDayOfWeek);
+    $lastDayOfMonth = $lastDayOfMonthInt->format('t');
+    $endDayOfWeek1 = date('Y-m-d', strtotime($firstDayOfWeek . ' + 6 days'));
+    if (date('m', strtotime($endDayOfWeek1)) != $month) {
+        $endDayOfWeek = date('Y-m-d', strtotime($lastDayOfMonth . "-" . $month . "-" . $year));
+    } else {
+        $endDayOfWeek = $endDayOfWeek1;
+    }
     return array($firstDayOfWeek, $endDayOfWeek);
 }
