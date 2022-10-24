bd98915d9b31bda4834632f061f2bf307c2e1962
css4dev <102645748+css4dev@users.noreply.github.com>
2022-10-24 14:39:21 +0300

    Update firstAndEndDateOfWeek.php

--- firstAndEndDateOfWeek.php
+++ firstAndEndDateOfWeek.php
@@ -1,11 +1,32 @@
-function firstAndEndDateOfWeek($date)
+<?php 
+function firstAndEndDateOfWeek($month, $weekNum, $year)
 {
-    $day = date('l', strtotime($date));
 
+    $firstDayOfMonth =$year. '-'. $month.'-01';
+
+    $week = weekOfYear($firstDayOfMonth); 
+  
+    //week num for first week of month
+    $weekNumOfYear = $week + $weekNum - 1;
+    
+    $firstDay = date('l', strtotime($year.'-01-01'));
     $daysOfWeek = array('Saturday', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday');
-    $dayOfWeek = array_search($day, $daysOfWeek) + 1;
-    $firstDayOfWeek = date('Y-m-d', strtotime($date . ' - ' . $dayOfWeek . ' days'));
-    $endDayOfWeek = date('Y-m-d', strtotime($firstDayOfWeek . ' + 6 days'));
-    //print_r(array($firstDayOfWeek,$endDayOfWeek));
-    return array($firstDayOfWeek, $endDayOfWeek);
+    $shift = 7 - (array_search($firstDay, $daysOfWeek) );
+    $dayOfYear = ($weekNumOfYear - 2) * 7 + $shift ;
+    $firstDayOfWeek1 = date('Y-m-d', strtotime($year."-01-01" . " + " . $dayOfYear    . " days"));
+   
+        $firstDayOfWeek = $firstDayOfWeek1;
+    
+    $lastDayOfMonthInt = new DateTime($firstDayOfWeek);
+    $lastDayOfMonth = $lastDayOfMonthInt->format('t');
+    $endDayOfWeek1 = date('Y-m-d', strtotime($firstDayOfWeek . ' + 6 days'));
+  
+        $endDayOfWeek = $endDayOfWeek1;
+        $allArray=array();
+        for( $i=0;$i<5;$i++){
+                $endDayOfWeek1 = date('Y-m-d', strtotime($firstDayOfWeek . ' + ' .$i .'days'));
+
+   array_push($allArray,$endDayOfWeek1);
+        }
+    return $allArray;
 }
